6594c04036634b334665aaf8ffb0e34b9e04d7ff
Colin <420115309@qq.com>
2019-04-18 21:50:41 +0800

    去除多余引用

--- src/main/java/com/colin/spring/springbucks/controller/exception/FormValidationException.java
+++ src/main/java/com/colin/spring/springbucks/controller/exception/FormValidationException.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
